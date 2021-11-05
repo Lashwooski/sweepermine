89f6ac2bd8b83c40b6e048ecb5ac1a17fccf4b1b
Lashwooski <53841671+Lashwooski@users.noreply.github.com>
2021-11-04 21:39:59 -0400

    Update MyFrame.java

--- MyFrame.java
+++ MyFrame.java
@@ -1,7 +1,10 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class MyFrame extends JFrame implements MouseListener {
 
@@ -17,23 +20,23 @@ public class MyFrame extends JFrame implements MouseListener {
     final double PROBABILITY = 0.16;
 
     MyFrame() {
+        addMouseListener(this);
         this.setLayout(null);
         this.setSize(500, 500);
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setBackground(Color.WHITE);
-        addMouseListener(this);
         for(int j = 0; j < SIZE; j++) {
             for(int i = 0; i < SIZE; i++) {
                 array[j][i] = new Square();
                 if (Math.random() < PROBABILITY) {
+                	mines++;
                     array[j][i].mine = true;
                 } else {
                     array[j][i].revealed = true;
                 }
             }
         }
-        getNumMines();
         countNumbers();
         repaint();
     }
@@ -41,20 +44,56 @@ public class MyFrame extends JFrame implements MouseListener {
     public void paint(Graphics g) {
         g.setColor(Color.WHITE);
         g.fillRect(0,0,500,500);
-        g.setColor(Color.GRAY);
+        g.setColor(Color.BLACK);
+        g.drawString("" + mines, 30, 43);
+        imageDraw(g, "flag", 10, 30);
         for (int j = 0; j < SIZE; j++) {
             for (int i = 0; i < SIZE; i++) {
+            	g.setColor(Color.GRAY);
                 if (array[j][i].mine) {
                     g.setColor(Color.RED);
                 }
-                g.fillRect(i * (X + 2) + 8, j * (Y + 2) + 31, X, Y);
+                g.fillRect(i * (X + 2) + 8, j * (Y + 2) + 51, X, Y);
                 if (array[j][i].revealed) {
                     g.setColor(Color.BLACK);
-                    g.drawString(String.valueOf(array[j][i].number), i * (X + 2) + 15, j * (Y + 2) + 49);
+                    g.drawString(String.valueOf(array[j][i].number), i * (X + 2) + 15, j * (Y + 2) + 69);
                 }
-                g.setColor(Color.GRAY);
             }
         }
+        imageDraw(g, "flag", xPos, yPos);
+    }
+
+    public void countNumbers() {
+        for (int y = 0; y < SIZE; y++) {
+            for (int x = 0; x < SIZE; x++) {
+                if (array[y][x].mine) continue;
+                for (int j = -1; j < 2; j++) {
+                    for (int i = -1; i < 2; i++) {
+                        if (y + j < 0 || y + j > SIZE - 1 || x + i < 0 || x + i > SIZE - 1) continue;
+                        if (array[y + j][x + i].mine) {
+                            array[y][x].number++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+        
+    public void imageDraw(Graphics g, String path, int x, int y) {
+        BufferedImage img = null;
+        try {
+        	java.net.URL url = Main.class.getResource("/images/" + path + ".png");
+            img = ImageIO.read(url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if(img != null) {
+        	g.drawImage(img, x, y, 20, 20, null);
+        }
+    }
+    
+    public BufferedImage cropImage(BufferedImage original, int x, int y) {
+    	return(original.getSubimage(x, y, 20, 20));
     }
 
     @Override
@@ -63,8 +102,8 @@ public class MyFrame extends JFrame implements MouseListener {
         yPos = e.getY();
         newX = (xPos - 8) / (X + 2);
         newY = (yPos - 31) / (Y + 2);
-
-
+        
+        
         repaint();
     }
 
@@ -87,30 +126,5 @@ public class MyFrame extends JFrame implements MouseListener {
     public void mouseExited(MouseEvent e) {
 
     }
-
-    public void getNumMines() {
-        for(int j = 0; j < SIZE; j++) {
-            for(int i = 0; i < SIZE; i++) {
-                if (array[j][i].mine) {
-                    mines++;
-                }
-            }
-        }
-    }
-
-    public void countNumbers() {
-        for (int y = 0; y < SIZE; y++) {
-            for (int x = 0; x < SIZE; x++) {
-                if (array[y][x].mine) continue;
-                for (int j = -1; j < 2; j++) {
-                    for (int i = -1; i < 2; i++) {
-                        if (y + j < 0 || y + j > SIZE - 1 || x + i < 0 || x + i > SIZE - 1) continue;
-                        if (array[y + j][x + i].mine) {
-                            array[y][x].number++;
-                        }
-                    }
-                }
-            }
-        }
-    }
+    
 }
